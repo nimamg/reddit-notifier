5022f91ec28d61ea5eb5c0f4653d998fe2a9c582
Nima Modares Gorji <nima.mg@gmail.com>
2024-09-04 00:06:48 -0700

    Add method to get new posts from reddit, save to db, and notify

--- reddit_wrapper.py
+++ reddit_wrapper.py
@@ -1,18 +1,29 @@
+from functools import lru_cache
+
 import praw
+from praw import Reddit
 
 from prawcore import NotFound
 
+from bot import BotWrapper
 from models import Submission
+from repository import RedditRepository, SubredditUserRepository
 
 
 class RedditWrapper:
-    def __init__(self, reddit: praw.Reddit, limit: int = 10):
+    def __init__(self, reddit: praw.Reddit, bot_wrapper, limit: int = 20):
         self.reddit = reddit
         self.limit = limit
+        self.latest_posts = {}
+        self.bot_wrapper: BotWrapper = bot_wrapper
 
     @property
     def subreddits(self):
-        return [sub['name'] for sub in self.config]
+        return RedditRepository.get_all_subreddits()
+
+    @lru_cache
+    def get_subreddits_from_db(self):
+        return RedditRepository.get_all_subreddits()
 
     def subreddit_exists(self, subreddit):
         try:
@@ -33,7 +44,29 @@ class RedditWrapper:
                 new_posts[subreddit] = [post for post in new_posts[subreddit] if post.created_utc > latest_post.created]
         return new_posts
 
-    # TODO: Periodically get new posts, save them to db, send them to respective users.
+    def get_latest_posts_from_db(self):
+        for subreddit in self.subreddits:
+            if subreddit.name not in self.latest_posts:
+                self.latest_posts[subreddit.name] = RedditRepository.get_latest_post_from_subreddit(subreddit)
+        return self.latest_posts
 
 
-reddit = RedditWrapper()
+    @staticmethod
+    def get_telegram_ids_per_subreddit():
+        ids = SubredditUserRepository.get_telegram_ids_per_subreddit()
+        return {r['name']: r['telegram_id'] for r in ids}
+
+    def get_latest_posts_from_reddit(self):
+        ids = self.get_telegram_ids_per_subreddit()
+        for subreddit in self.subreddits:
+            new_posts = list(self.reddit.subreddit(subreddit.name).new(limit=self.limit))
+            for post in reversed(new_posts):
+                if post.created_utc > self.latest_posts[subreddit.name].created:
+                    self.latest_posts[subreddit.name] = post.created_utc
+                    RedditRepository.create_submission(post.id, post.title, subreddit, post.created_utc, post.url, post.permalink)
+                    for _id in ids[subreddit.name]:
+                        self.bot_wrapper.send_message(_id, f'New post in {subreddit.name}:\n{post.title}\n{post.url}')
+
+
+
+reddit = RedditWrapper(Reddit('bot1'))
